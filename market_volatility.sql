05838103766e9b0c69492cb9ee5a0f5e0c082867
Noha-Jung3 <jungnoha@gmail.com>
2026-08-25 17:22:03 +1000

    queries for monitoring pipeline

--- market_volatility.sql
+++ market_volatility.sql
@@ -36,6 +36,7 @@ CREATE TABLE Predictions(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 );
+-- "what did the model predict?"
 
 SELECT MAX("Date")
 FROM engineered_features;
@@ -52,3 +53,37 @@ SELECT *
 FROM predictions
 ORDER BY created_at DESC;
 
+CREATE TABLE prediction_performance (
+    prediction_date DATE PRIMARY KEY,
+
+    predicted_vol_10 DOUBLE PRECISION,
+
+    actual_vol_10 DOUBLE PRECISION,
+
+    error DOUBLE PRECISION,
+
+    absolute_error DOUBLE PRECISION,
+
+    squared_error DOUBLE PRECISION,
+
+    model_name TEXT,
+
+    evaluated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- "How well did the model perform?"
+-- all three errors? -> error lets us know if prediction was over predicted or under predicted
+-- absolute_error lets us know about the magnitude of the error, and squared_error is useful for calculating MSE or RMSE
+
+SELECT *
+FROM prediction_performance
+
+SELECT *
+FROM prediction_performance
+ORDER BY prediction_date DESC;
+
+SELECT
+    prediction_date,
+    predicted_vol_10,
+    actual_vol_10
+FROM prediction_performance
+WHERE prediction_date = '2026-07-28';
